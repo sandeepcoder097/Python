ed5e149ce14a1c3908f48a470eedd7de1815e304
Anshulcodes <31853258+Anshulcodes@users.noreply.github.com>
2018-04-01 22:30:09 +0530

    Add files via upload

--- game.py
+++ game.py
@@ -1,17 +1,33 @@
 import random
 
 
+def keygen():
+    keyl= 'A'
+    keyh= 'z'
+    return random.randrange(ord(keyl),ord(keyh))
+
+def protect():
+    shieldl= int(90)
+    shieldh= int(108)
+    return random.randrange(shield1,shieldh)
+    
 def destroy():
     hp=500
+    gain=95
     atkl=100
     atkh=120
     while hp >0:
+        key= chr(keygen())
+        print(key)
+        match= input('Press')
+        if key== match:
+            hp= hp + gain
         dmg= random.randrange(atkl,atkh)
         hp= hp - dmg
         print('Health of the player is',hp,'and the damage is',dmg)
-        if hp >90:
+        if hp < 120:
             print('You have been transferred to the rest room as your health is critically low.')
-        elif hp <90:
+        elif hp < 90:
             print('You died')
             break
     return hp
